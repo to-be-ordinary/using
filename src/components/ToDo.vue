559b909d334b1cb0ce7fc00ac42c7cb9201e04c2
sunmengjie <2586354329@qq.com>
2018-07-12 11:21:49 +0800

    初始导入代码

---
+++ src/components/ToDo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input v-model="inputValue">
+        <button @click="handleSubmit">提交</button>
+        <ul>
+            <item v-for="(item, index) of list" :key="index" :item="item" :index="index" @delete="handleDelete"></item>
+        </ul>
+    </div>
+</template>
+<script>
+import todoList from './ToDoList'
+
+export default {
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  components: {
+    item: todoList
+  },
+  methods: {
+    handleSubmit: function () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete: function (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,16 @@
+<template>
+    <li @click="clickHandle">{{item}}</li>
+</template>
+<script>
+export default {
+  props: [
+    'item',
+    'index'
+  ],
+  methods: {
+    clickHandle: function () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
